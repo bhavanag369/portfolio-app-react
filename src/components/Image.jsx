15f9d3b4c5a3938b4a337c23db64ae2a154775a2
gbooo <gb00000009@gmail.com>
2023-05-28 00:23:43 +0530

    final commit

---
+++ src/components/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import resume from '../assets/resume.png'
+const Image = () => {
+  return (
+    <div><img src={resume} alt="resume" width='100%' height='100%' /></div>
+  )
+}
+
+export default Image
--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,41 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
-
+import s from '../assets/projects/s.png'
+import sorting from '../assets/projects/sorting.jpeg'
+import routefinding from '../assets/projects/routefinding.jpeg'
 export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
+    // {
+    //     id:1,
+    //     name:"React JS Application",
+    //     image:WorkIm,
+    //     github:"",
+    //     live:"",
+    // },
     {
         id:2,
         name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        image:routefinding,
+        github:"https://github.com/bhavanag369/routefinding-visualization/tree/main/routefinding_visualization",
+        live:"https://6472373f2837561b70a2e8de--monumental-douhua-36ba8a.netlify.app/",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name: "Vanilla JS Application",
+        image: sorting,
+        github: "https://github.com/bhavanag369/sorting-visualization/tree/main/sorting",
+        live: "https://647238f25889951baf239c43--courageous-axolotl-9ac106.netlify.app/",
+        
     },
     {
         id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name: "php python Application",
+        image: s,
+        github: "",
+        live: "",
+        
     },
+
+    
 
 
 ]
